23b34d6bc4c53dd83f1bb6b7416824ab72921b48
Daniele Cencig <daniele.cencig@outlook.com>
2023-06-09 12:36:47 +0200

    debug queries only on env flag enable

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,7 +1,8 @@
-import { dev } from '$app/environment';
+import { DEBUG_PRISMA } from '$env/static/private';
 import { PrismaClient } from '@prisma/client';
 
-const prismaClient = global.prisma || new PrismaClient({ log: dev ? ['query', 'info', 'warn', 'error'] : [] });
+const prismaClient =
+  global.prisma || new PrismaClient({ log: DEBUG_PRISMA === 'true' ? ['query', 'info', 'warn', 'error'] : [] });
 
 if (process.env.NODE_ENV === 'development') {
   global.prisma = prismaClient;
